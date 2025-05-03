cbbd069ed3155384e8d47c520318ca37f4ca6c8e
Allan Dumaine <allan.dumaine@gmail.com>
2025-05-03 12:32:01 -0700

    Change Horizen 2 to Base

--- packages/zenclaim-signtool/signtool.js
+++ packages/zenclaim-signtool/signtool.js
@@ -22,7 +22,7 @@ arguments:
 ${'Short forms of arguments'.cyan} 
   -pk="" -ms="" -cp= -nt="" -s -h -v
 ${'Claiming ZEN:'.cyan}
-The message to sign should consist of the word ZENCLAIM and the destination address on Horizen 2
+The message to sign should consist of the word ZENCLAIM and the destination address on Base
   Example "ZENCLAIM0x1448283357e8FB6EA763a78836FFD5517149BF70"
 See the multisig claim tool for the message to sign for multisig addresses. That tool can generate the message to sign for you.
 `;
