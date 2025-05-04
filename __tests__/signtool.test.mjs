56cb49474383a55e2e1dc2d2b08cfa44472999b9
Allan Dumaine <allan.dumaine@gmail.com>
2025-05-04 13:54:47 -0700

    Fix  signtool test
    Fix result var to always be in scope of test
    clean up const naming
    add tests for testnet and raw private key

--- __tests__/signtool.test.mjs
+++ __tests__/signtool.test.mjs
@@ -3,6 +3,8 @@ import { signMessage } from '../bin/signtool.js';
 
 describe('signtool.js', () => {
   const privKey = 'KzFBhL99mZDTdjMhX4jMnybVxkTTACLm6gq9veUjpXcQuXbaAnHf';
+  const privKeyRaw = "5a4548fe4ce6a4ae9b23eda6f29ec2078aac1dcd553e0b31c28ff1154d73dd19";
+  const privKeyTestnet = 'cSebvnBKGL9CKKj9Tks5faCbPJXHxv8nzrAg32RragJNoLWtgo26';
   const message = "ZENCLAIM0x0Fd343F9a263906bD6AfebfDD4011579979E8aeC";
   const messageTestnet = "ZT1CLAIM0x0Fd343F9a263906bD6AfebfDD4011579979E8aeC";
   const compressed = true;  //default
@@ -11,44 +13,60 @@ describe('signtool.js', () => {
   const multisigMessage ='ZT1CLAIM0x7caa11b3e0cdf22e9af9a4c5ac1cdc80938c34180x0Fd343F9a263906bD6AfebfDD4011579979E8aeC';
 
   // response data;
-  let result = null;
-  let isValid = null;
-  const expectedAddressTestnet = 'ztXzHcS1UVvcXd2AkKL5FYV4QuvWn9ix16C';
-  const expectedSig = 'IHe0yZyQfLbsA19QZf1i8get70ckrRI4FHDRF8A6dHCxYUVJ/us3PdViOnc/7zoBGoCGR65EVCQl7KI9f9IuQXA=';
-  const expectedAddressMainnet = 'znVvEqoY9rDfPMX5Sx35UGbycUMedUrHJ6q';
-  const expectedSigCompressedFalse = 'IGVVfimVlXoZgv/W8z2BWehZ42IC8Idf4UogyV7VW0MUIeMADEP4bJlVjJScVegXE3RT11aHCROqCcyq4EG+Hec=';
-  const expectedSigMultisig = 'Hxz8yEDGwf7fXuYEKKmoQIf5TeYQaA2Gga2BOwggAQTLJWeRlGThLr8y1jiwgJ+QxudvYXIAo2OG+PhT9AQtAbQ=';
-  // const expectedAddressCompressedFalse = 'zthgXCY4dy3M8jvZYHr7yNNaF9KYR24ACfb';
+  const expectedAddressTestnet = 'ztfPyquDswoUBUBNNWqcPeZrpD7x7QSE9Zt';
+  const expectedSigTestnet = 'G1wd3KLH7bAXxuUfloCGuOVIdEkanj9UKQQuW3Y5dKV/LMpa/qsPrQGAsZPemyNqfu1YLbZ8jJhP2/hHLI2Z8Yc=';
+  const expectedSig = 'HHe0yZyQfLbsA19QZf1i8get70ckrRI4FHDRF8A6dHCxYUVJ/us3PdViOnc/7zoBGoCGR65EVCQl7KI9f9IuQXA=';
+  const expectedAddressMainnet = 'znfcURubKKLPzURUEvZ8C6VVShkgGQjeu6X';
+  const expectedAddressMainnetAlt = 'znVvEqoY9rDfPMX5Sx35UGbycUMedUrHJ6q';
+  const expectedAddressCompressedFalse ='znfcURubKKLPzURUEvZ8C6VVShkgGQjeu6X';
+  const expectedSigCompressedFalse= 'HHe0yZyQfLbsA19QZf1i8get70ckrRI4FHDRF8A6dHCxYUVJ/us3PdViOnc/7zoBGoCGR65EVCQl7KI9f9IuQXA='
+  const expectedSigRaw= 'IHe0yZyQfLbsA19QZf1i8get70ckrRI4FHDRF8A6dHCxYUVJ/us3PdViOnc/7zoBGoCGR65EVCQl7KI9f9IuQXA='
+  const expectedSigMultisig = 'Gxz8yEDGwf7fXuYEKKmoQIf5TeYQaA2Gga2BOwggAQTLJWeRlGThLr8y1jiwgJ+QxudvYXIAo2OG+PhT9AQtAbQ=';
 
   test('signMessage should reject a missing or invalid message', () => {
-    isValid = signMessage({ privKey});
+    const isValid = signMessage({ privKey, network});
     expect(typeof isValid).toBe('object');
     expect(isValid).toHaveProperty('error');
   });
   test('signMessage should reject an invalid private key', () => {
-    isValid = signMessage({message, privKey:'123456'});
+    const isValid = signMessage({message, privKey:'123456'});
+    expect(typeof isValid).toBe('object');
     expect(isValid).toHaveProperty('error');
   });
   test('signMessage should return a mainnet address on an invalid testnet', () => {
-    result = signMessage({message, privKey, compressed, network:'invalid testnet'});
+    const result = signMessage({message, privKey, compressed, network:'invalid testnet'});
     expect(typeof result).toBe('object');
     expect(result).toHaveProperty('address');
-    expect(result.address).toBe(expectedAddressMainnet);
+    expect(result.address).toBe(expectedAddressMainnetAlt);
   });
-  test('signMessage should return a alternate message and address when commpressed is false', () => {
-    result = signMessage({message: messageTestnet, privKey, compressed: false, network, verbose});
+  test('signMessage should return a different signature and address when commpressed is false', () => {
+    const result = signMessage({message, privKey, compressed: false, verbose});
     expect(typeof result).toBe('object');
-    expect(result.address).toBe(expectedAddressTestnet);
+    expect(result.address).toBe(expectedAddressCompressedFalse);
     expect(result.signature).toBe(expectedSigCompressedFalse);
   });
   test('signMessage should sign a message and return an object with signature and address', () => {
-    result = signMessage({message, privKey})
+    const result = signMessage({message, privKey});
+    console.log(result);
     expect(typeof result).toBe('object');
     expect(result.signature).toBe(expectedSig);
     expect(result.address).toBe(expectedAddressMainnet);
   });
+  test('signMessage should sign a message and return an object with testnet signature and address', () => {
+    const result = signMessage({message: messageTestnet, privKey:privKeyTestnet, network:"testnet", verbose});
+    console.log(result);
+    expect(typeof result).toBe('object');
+    expect(result.signature).toBe(expectedSigTestnet);
+    expect(result.address).toBe(expectedAddressTestnet);
+  });
+  test('signMessage should sign a message with a raw key and return an object with signature and address', () => {
+    const result = signMessage({message, privKey: privKeyRaw, network: "mainnet", compressed, verbose})
+    expect(typeof result).toBe('object');
+    expect(result.signature).toBe(expectedSigRaw);
+    expect(result.address).toBe(expectedAddressMainnetAlt);
+  });
   test('signMessage should sign a multisig formatted message and return an object with signature and address', () => {
-    result = signMessage({message: multisigMessage, privKey, verbose})
+    const result = signMessage({message: multisigMessage, privKey, verbose})
     expect(typeof result).toBe('object');
     expect(result.signature).toBe(expectedSigMultisig);
     expect(result.address).toBe(expectedAddressMainnet);
