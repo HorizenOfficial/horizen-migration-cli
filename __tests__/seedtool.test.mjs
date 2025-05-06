7bdee6199d81d8a9222e09fa17829376f05c46cf
Allan Dumaine <allan.dumaine@gmail.com>
2025-05-06 09:31:59 -0700

    Formatting/linting in seedtool test
    remove console.log

--- __tests__/seedtool.test.mjs
+++ __tests__/seedtool.test.mjs
@@ -3,33 +3,33 @@ import { deriveAddresses } from '../bin/seedtool.js';
 
 describe('seedtool.js', () => {
   const validOptions = {
-   mnemonicPhrase: 'used normal chronic write traffic volume sting oxygen cluster magic era strategy avocado',
-   verbose: false,
-   numAddresses: 2,
-   network: "testnet",
- }
- const derivationPath = "m/44'/121'/5'/0/";
- const expectedDPAddr = {address: 'ztdXJrR18UnBxcBbn32CsotqwDJz87DK4WS'}
- const expectedArray =  [
-  {
-    derivationPath: "m/44'/121'/0'/0/0",
-    address: 'ztVPyDhrqYcWXdUZg7EjVQ2yW5mXEirtEH7',
-    pubKey: '03aa4b570ebb941e34ee13311b963d55d3c307c933f80a10d94d87eec63f4ede83',
-    addressUncompressed: 'ztfPyquDswoUBUBNNWqcPeZrpD7x7QSE9Zt',
-    pubKeyUncompressed: '04aa4b570ebb941e34ee13311b963d55d3c307c933f80a10d94d87eec63f4ede834d8b4b2eb5293da33ef525b0ab29f10598927444bf5a1f809f5cd958bec41be7',
-    privKeyRaw: '9731a9ada5a46dfb9250123f8150aec097d4572226f4d7c0fc03781a270bd2fc',
-    privKeyWIF: 'cSebvnBKGL9CKKj9Tks5faCbPJXHxv8nzrAg32RragJNoLWtgo26'
-  },
-  {
-    derivationPath: "m/44'/121'/0'/0/1",
-    address: 'ztbnrP5hcTJsHBJGUKHHVJZp1JMuidPeF7Y',
-    pubKey: '020f7c9d87afd715e314e32a857e67db6a62eb833b2d01f9aea5d6827d3a1bc3fb',
-    addressUncompressed: 'ztYBYuBCKUxfbxybqJyQWxoZATvPWdmaXuP',
-    pubKeyUncompressed: '040f7c9d87afd715e314e32a857e67db6a62eb833b2d01f9aea5d6827d3a1bc3fb51b16de454c6f6268038117aa25e602b27d5f25a4eb446a08eaf8f1654e8d3f6',
-    privKeyRaw: 'b8955b4bb6d08b4c8181cef89b2ba13ee25a37bf62c7a4e52b9f08d81d2546bf',
-    privKeyWIF: 'cTmWQ9RmmN1Jb1FucFaoE7YTGMhvy3PDQ1wUzo13hLCimWjcXyEw'
+    mnemonicPhrase: 'used normal chronic write traffic volume sting oxygen cluster magic era strategy avocado',
+    verbose: false,
+    numAddresses: 2,
+    network: "testnet",
   }
-]
+  const derivationPath = "m/44'/121'/5'/0/";
+  const expectedDPAddr = { address: 'ztdXJrR18UnBxcBbn32CsotqwDJz87DK4WS' }
+  const expectedArray = [
+    {
+      derivationPath: "m/44'/121'/0'/0/0",
+      address: 'ztVPyDhrqYcWXdUZg7EjVQ2yW5mXEirtEH7',
+      pubKey: '03aa4b570ebb941e34ee13311b963d55d3c307c933f80a10d94d87eec63f4ede83',
+      addressUncompressed: 'ztfPyquDswoUBUBNNWqcPeZrpD7x7QSE9Zt',
+      pubKeyUncompressed: '04aa4b570ebb941e34ee13311b963d55d3c307c933f80a10d94d87eec63f4ede834d8b4b2eb5293da33ef525b0ab29f10598927444bf5a1f809f5cd958bec41be7',
+      privKeyRaw: '9731a9ada5a46dfb9250123f8150aec097d4572226f4d7c0fc03781a270bd2fc',
+      privKeyWIF: 'cSebvnBKGL9CKKj9Tks5faCbPJXHxv8nzrAg32RragJNoLWtgo26'
+    },
+    {
+      derivationPath: "m/44'/121'/0'/0/1",
+      address: 'ztbnrP5hcTJsHBJGUKHHVJZp1JMuidPeF7Y',
+      pubKey: '020f7c9d87afd715e314e32a857e67db6a62eb833b2d01f9aea5d6827d3a1bc3fb',
+      addressUncompressed: 'ztYBYuBCKUxfbxybqJyQWxoZATvPWdmaXuP',
+      pubKeyUncompressed: '040f7c9d87afd715e314e32a857e67db6a62eb833b2d01f9aea5d6827d3a1bc3fb51b16de454c6f6268038117aa25e602b27d5f25a4eb446a08eaf8f1654e8d3f6',
+      privKeyRaw: 'b8955b4bb6d08b4c8181cef89b2ba13ee25a37bf62c7a4e52b9f08d81d2546bf',
+      privKeyWIF: 'cTmWQ9RmmN1Jb1FucFaoE7YTGMhvy3PDQ1wUzo13hLCimWjcXyEw'
+    }
+  ]
 
   // returns array of objects or {error: message}
   test('deriveAddresses should return an error for missing mnemonicPhrase', async () => {
@@ -54,7 +54,7 @@ describe('seedtool.js', () => {
     const result = await deriveAddresses(options);
     expect(result).toHaveLength(5);
   });
-  test('deriveAddresses should return an error for invalid network value', async() => {
+  test('deriveAddresses should return an error for invalid network value', async () => {
     const invalidOptions = { ...validOptions, network: 'invalid' };
     const result = await deriveAddresses(invalidOptions);
     expect(result.every(item => item.address.startsWith('zn'))).toBe(true);
@@ -62,18 +62,17 @@ describe('seedtool.js', () => {
   test('deriveAddresses should return address from alternate derivation paths', async () => {
     const compressedOptions = { ...validOptions, derivationPath: "m/44'/121'/5'/0/" };
     const result = await deriveAddresses(compressedOptions);
-    expect(result.every(item => item.derivationPath.startsWith(derivationPath))).toBe(true); // Assuming testnet addresses start with 't'
+    expect(result.every(item => item.derivationPath.startsWith(derivationPath))).toBe(true);
     expect.arrayContaining([expect.objectContaining(expectedDPAddr)]);
   });
-
-  test('deriveAddresses should return a string for any stringify value', async() => {
+  test('deriveAddresses should return a string for any stringify value', async () => {
     const invalidOptions = { ...validOptions, stringify: 'true' };
     const result = await deriveAddresses(invalidOptions);
-    expect(typeof result).toBe('string'); 
+    expect(typeof result).toBe('string');
   });
+
   test('deriveAddresses should return an object with the correct structure for valid options', async () => {
     const result = await deriveAddresses(validOptions);
-    console.log(result);
     expect(typeof result).toBe('object');
     expect(result.length).toEqual(validOptions.numAddresses);
     expect(result[0].derivationPath).toBe(expectedArray[0].derivationPath);
